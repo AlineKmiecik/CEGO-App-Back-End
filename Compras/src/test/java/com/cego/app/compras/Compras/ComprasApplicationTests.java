3d221e97364130ac360d92903241ccf114ba6339
Aline Kmiecik <alinekmiecik@hotmail.com>
2024-01-02 19:05:08 -0300

    Aplicação modelo para implementação de funcionalidades

---
+++ Compras/src/test/java/com/cego/app/compras/Compras/ComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cego.app.compras.Compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
